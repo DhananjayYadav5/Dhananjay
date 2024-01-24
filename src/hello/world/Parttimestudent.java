9194a4c1a3ba62cbba89ed9a8af4b110cc95036e
sys <sys@DESKTOP-H5EPEHE>
2024-01-24 14:38:53 -0500

    added comit1

---
+++ src/hello/world/Parttimestudent.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author sys
+ */
+public class Parttimestudent {
+   private int numofCourses; 
+
+    public int getNumofCourses() {
+        return numofCourses;
+    }
+
+    public Parttimestudent(int numofCourses) {
+        this.numofCourses = numofCourses;
+    }
+}
